d914597b423289e4931adf7af1ae33426aaa9d8a
irie-shinnosuke <irie-shinnosuke@kayac.com>
2018-02-28 19:36:20 +0900

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+import {Howl, Howler} from 'howler';
+const sleep = new Howl({
+    src: ['./../sound/sleep.mp3'],
+    loop: true
+})
+const move = new Howl({
+    src: ['./../sound/move.wav']
+})
+
+
+let flag = false;
+let counter = 0;
+const dom = document.querySelector('.js-hoge');
+window.addEventListener('devicelight', (event) => {
+    const value = event.value
+    if(value > 2) {
+        dom.innerHTML = `${value}: 起きている`;
+        if(flag) {
+            move.play()
+        }
+        sleep.stop()
+    } else {
+        dom.innerHTML = `${value}: 眠っている`;
+        flag = true;
+        sleep.play()
+    }
+})
